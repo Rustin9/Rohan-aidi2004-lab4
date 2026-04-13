6b998a88b1639b8ee0e5e5c214abc15b853c1acf
RohanSaldanha <rohan.saldanha@dcmail.ca>
2026-04-12 22:12:40 -0400

    Add greeting app by Rohan

---
+++ app.py
@@ -0,0 +1,8 @@
+def greet (name):
+    return f"Hello , {name}! Welcome to AIDI 2004."
+def farewell (name):
+    return f" Goodbye , {name}! Keep coding !"
+if __name__ == "__main__":
+    print(greet("Rohan"))
+    print(farewell("Rohan"))
+print('EOF')
